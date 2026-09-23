968cd7d7fc84093f966e4e3c04ded5860d74ff5c
Lorenzo-39 <lorenzopolycarpo@gmail.com>
2026-09-23 07:53:10 -0300

    Update ReservaRepository.java

--- backend/src/main/java/com/reservadesalas/backend/repository/ReservaRepository.java
+++ backend/src/main/java/com/reservadesalas/backend/repository/ReservaRepository.java
@@ -1,5 +1,11 @@
 package com.reservadesalas.backend.repository;
 
-public class ReservaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reservadesalas.backend.model.Reserva;
+
+@Repository 
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     
 }
